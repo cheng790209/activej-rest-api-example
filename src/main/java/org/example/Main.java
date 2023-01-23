d5ca698b4b0018532d9d21f9d7059188a4e73aa3
Raul Armas <r4rmas@gmail.com>
2023-01-23 09:42:46 -0500

    Create router

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import io.activej.http.AsyncServlet;
+import static io.activej.http.HttpMethod.GET;
 import io.activej.http.HttpResponse;
+import io.activej.http.RoutingServlet;
 import io.activej.inject.annotation.Provides;
 import io.activej.launcher.Launcher;
 import io.activej.launchers.http.HttpServerLauncher;
@@ -10,9 +12,11 @@ import io.activej.promise.Promise;
 public class Main extends HttpServerLauncher {
     @Provides
     AsyncServlet servlet() {
-        return request -> Promise.of(
-                HttpResponse.ok200()
-                    .withJson("{ message: \"It was sent from an Activej API Rest\", author: \"Raul Armas\" }"));
+        return RoutingServlet.create()
+                .map(GET, "/", request -> HttpResponse.ok200()
+                        .withJson("{ message: \"It was sent from an Activej Rest API\", author: \"Raul Armas\" }"))
+                .map(GET, "/login", request -> HttpResponse.ok200()
+                        .withJson("{ message: \"Use your email and password to log in\" }"));
     }
 
     public static void main(String[] args) throws Exception {
