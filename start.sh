1db6bd8475fdeecc09266c5f83a1d0d9756c8827
cheng790209 <cheng790209@gmail.com>
2023-12-06 08:09:38 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export JAVA_TOOL_OPTIONS='-javaagent:/tmp/opentelemetry-javaagent.jar' \
        OTEL_EXPORTER_OTLP_ENDPOINT='http://server:4318' \
-       OTEL_EXPORTER_OTLP_HEADERS='authorization=194f9b8d-314c-4057-9248-2851e1625316' \
+       OTEL_EXPORTER_OTLP_HEADERS='authorization=<key>' \
        OTEL_EXPORTER_OTEL_PROTOCOL='http/protobuf' \
        OTEL_LOGS_EXPORTER=oltp \
        OTEL_SERVICE_NAME=activej_test \
